aeaa928df698e5f8a706bb212d1261cb7eeddb14
Vladimir Tomic <vladimir.tomic@swisscom.com>
2021-11-09 16:54:04 +0100

    Add a header and some CSS animations for hovering over a TODO

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
 import Todos from "./components/Todos";
 
 function App() {
   return (
     <div>
+      <Header />
       <Todos />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import "../css/Header.css";
+
+const Header = () => {
+    return (
+        <div className="header">
+            TODO Tracker
+        </div>
+    );
+}
+export default Header;
