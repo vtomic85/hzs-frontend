752036a4ca0b412cfa7fa8c2e7c94f199ba6463b
Vladimir Tomic <vladimir.tomic@swisscom.com>
2021-11-09 10:46:38 +0100

    Introduce useState and pass a function to a component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import Greeting from "./components/Greeting";
 
 function App() {
+  const [age, setAge] = useState(36);
+
+  const incrementAge = () => {
+    setAge(age + 1);
+  }
+
   return (
     <div>
-      <Greeting firstName="Vladimir" lastName="Tomic" />
+      <Greeting firstName="Vladimir" lastName="Tomic" age={age} onAgeIncremented={incrementAge} />
     </div>
   );
 }
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,7 +1,9 @@
 const Greeting = (props) => {
     return (
         <div>
-            Hello, {props.firstName} {props.lastName}, nice to meet you!
+            <p>Hello, {props.firstName} {props.lastName}, nice to meet you!</p>
+            <p>You are {props.age} years old.</p>
+            <button onClick={props.onAgeIncremented}>Click here to age up</button>
         </div>
     );
 }
