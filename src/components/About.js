b80bda8aed7cb355441fc2ac20c995d563734aff
vtomic85 <vladimir.tomic.la@gmail.com>
2021-11-21 20:32:06 +0100

    Add About page

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <>
+            <h1>About</h1>
+            <p>This application was developed for the purposes of FONIS Hackathon Frontend Workshop</p>
+            <p>Author: Vladimir Tomic</p>
+        </>
+    );
+}
+export default About;
