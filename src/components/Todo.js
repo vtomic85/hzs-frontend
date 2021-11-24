c48d854327578c172aee3229f6448339118a32b7
vtomic85 <vladimir.tomic.la@gmail.com>
2021-11-24 20:01:43 +0100

    Test

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,29 +1,29 @@
 import "../css/Todo.css";
-import {AiFillDelete} from 'react-icons/ai';
+import { AiFillDelete } from "react-icons/ai";
 
-const Todo = ({todo, todoClicked, deleteTodo}) => {
-    const handleDelete = (e) => {
-        e.stopPropagation();
-        deleteTodo(todo);
-    }
+const Todo = ({ todo, todoClicked, deleteTodo }) => {
+  const handleDelete = (e) => {
+    e.stopPropagation();
+    deleteTodo(todo);
+  };
 
-    return (
-        <div className="todo-container" onClick={() => todoClicked(todo)}>
-            <div className={"todo-content" + (todo.isDone ? " done" : "")}>
-                <div>
-                    <div className="todo-h3-title">{todo.title}</div>
-                    <span className="todo-type">{todo.todoItemType}</span>
-                </div>
-                <div>
-                    <p className="todo-description">{todo.description}</p>
-                </div>
-            </div>
-            <div className={"todo-status" + (todo.isDone ? " done" : "")}>
-                {todo.isDone ? "Done" : "Not done"}
-                <AiFillDelete className="delete-icon" onClick={handleDelete}/>
-            </div>
+  return (
+    <div className="todo-holder" onClick={() => todoClicked(todo)}>
+      <div className={"todo-content" + (todo.isDone ? " done" : "")}>
+        <div>
+          <div className="todo-h3-title">{todo.title}</div>
+          <span className="todo-type">{todo.todoItemType}</span>
         </div>
-    );
-}
+        <div>
+          <p className="todo-description">{todo.description}</p>
+        </div>
+      </div>
+      <div className={"todo-status" + (todo.isDone ? " done" : "")}>
+        {todo.isDone ? "Done" : "Not done"}
+        <AiFillDelete className="delete-icon" onClick={handleDelete} />
+      </div>
+    </div>
+  );
+};
 
 export default Todo;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,62 +1,70 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import TodoList from "./TodoList";
 import NewTodoForm from "./NewTodoForm";
+import "../css/Todos.css";
 
 const Todos = () => {
-    const BASE_URL = 'https://hzs-todo.herokuapp.com/todo-items/';
-    const [todos, setTodos] = useState([]);
+  const BASE_URL = "https://hzs-todo.herokuapp.com/todo-items/";
+  const [todos, setTodos] = useState([]);
 
-    const getData = () => {
-        fetch(BASE_URL)
-            .then(response => response.json())
-            .then(data => setTodos(data));
-    }
+  const getData = () => {
+    fetch(BASE_URL)
+      .then((response) => response.json())
+      .then((data) => setTodos(data));
+  };
 
-    const onTodoDeleted = (deletedTodo) => {
-        let newTodos = todos.filter(todo => todo.id !== deletedTodo.id);
-        setTodos(newTodos);
-        fetch(BASE_URL + deletedTodo.id, {
-            method: 'DELETE',
-        });
-    }
+  const onTodoDeleted = (deletedTodo) => {
+    let newTodos = todos.filter((todo) => todo.id !== deletedTodo.id);
+    setTodos(newTodos);
+    fetch(BASE_URL + deletedTodo.id, {
+      method: "DELETE",
+    });
+  };
 
-    useEffect(() => getData(),
-        []);
+  useEffect(() => getData(), []);
 
-    const onTodoClicked = (todo) => {
-        let newTodo = {...todo, isDone: !todo.isDone};
-        let newTodos = todos.map(todo => todo.id === newTodo.id ?
-            {...todo, isDone: !todo.isDone} :
-            todo);
-        setTodos(newTodos);
-        const path = `${BASE_URL}/${newTodo.id}/${newTodo.isDone ? 'done' : 'not-done'}`;
-        fetch(path, {
-            method: 'PUT'
-        });
-    }
-
-    const onTodoAdded = (todo) => {
-        let newTodos = [...todos, todo];
-        setTodos(newTodos);
-        fetch(BASE_URL, {
-            method: 'POST',
-            body: JSON.stringify({
-                title: todo.title,
-                description: todo.description,
-                todoItemType: todo.todoItemType,
-                isDone: todo.isDone
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        })
-    }
-
-    return (
-        <>
-            <NewTodoForm onTodoAdded={(todo) => onTodoAdded(todo)}/>
-            <TodoList todos={todos} onTodoClicked={onTodoClicked} onTodoDeleted={onTodoDeleted}/>
-        </>
+  const onTodoClicked = (todo) => {
+    let newTodo = { ...todo, isDone: !todo.isDone };
+    let newTodos = todos.map((todo) =>
+      todo.id === newTodo.id ? { ...todo, isDone: !todo.isDone } : todo
     );
-}
+    setTodos(newTodos);
+    const path = `${BASE_URL}/${newTodo.id}/${
+      newTodo.isDone ? "done" : "not-done"
+    }`;
+    fetch(path, {
+      method: "PUT",
+    });
+  };
+
+  const onTodoAdded = (todo) => {
+    let newTodos = [...todos, todo];
+    setTodos(newTodos);
+    fetch(BASE_URL, {
+      method: "POST",
+      body: JSON.stringify({
+        title: todo.title,
+        description: todo.description,
+        todoItemType: todo.todoItemType,
+        isDone: todo.isDone,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+  };
+
+  return (
+    <>
+      <NewTodoForm onTodoAdded={(todo) => onTodoAdded(todo)} />
+      <div className="todo-container">
+        <TodoList
+          todos={todos}
+          onTodoClicked={onTodoClicked}
+          onTodoDeleted={onTodoDeleted}
+        />
+      </div>
+    </>
+  );
+};
 export default Todos;
