d1bef2e5254239b3f425ead12e661e120de131b9
vtomic85 <vladimir.tomic.la@gmail.com>
2021-11-24 11:23:55 +0100

    Add React Router for simple navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import "./css/App.css";
 import Header from "./components/Header";
 import Todos from "./components/Todos";
+import About from "./components/About";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-    return (
-        <div className="container">
-            <Header/>
-            <Todos/>
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <nav>
+          <Link className="link" to="/">
+            TODO List
+          </Link>
+          <Link className="link" to="/about">
+            About
+          </Link>
+        </nav>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Todos />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
