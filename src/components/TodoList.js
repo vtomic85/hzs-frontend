a46de1bb04fbc0b19938fcaabd310c875015ed96
vtomic85 <vladimir.tomic.la@gmail.com>
2021-11-25 12:39:03 +0100

    Separate rendering of TodoList if the TODOs are loaded and if they are not

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,26 @@
 import Todo from "./Todo";
 
-const TodoList = ({todos, onTodoClicked, onTodoDeleted}) => {
-    if (todos) {
-        return todos.map((todo, i) =>
+const TodoList = ({ todos, onTodoClicked, onTodoDeleted }) => {
+  return (
+    <>
+      {
+        // If there are todos...
+        todos &&
+          todos.map((todo, i) => (
             <Todo
-                key={i}
-                todo={todo}
-                todoClicked={(todo) => onTodoClicked(todo)}
-                deleteTodo={(todo) => onTodoDeleted(todo)}
-            />)
-    } else {
-        return <p>Loading...</p>
-    }
-
-}
+              key={i}
+              todo={todo}
+              todoClicked={(todo) => onTodoClicked(todo)}
+              deleteTodo={(todo) => onTodoDeleted(todo)}
+            />
+          ))
+      }
+      {
+        // If there are no todos (yet)...
+        !todos && <p>Loading...</p>
+      }
+    </>
+  );
+};
 
 export default TodoList;
