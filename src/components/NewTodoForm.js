d52a168711ff201db387309ef48af4295adff9bc
vtomic85 <vladimir.tomic.la@gmail.com>
2021-11-26 15:10:42 +0100

    Reset the form upon adding new TODO

--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -24,6 +24,9 @@ const NewTodoForm = ({ onTodoAdded }) => {
       description: newTodoDescription,
       todoItemType: newTodoType,
     });
+    setNewTodoType("WORK");
+    setNewTodoTitle("");
+    setNewTodoDescription("");
   };
 
   return (
